bbf7a46e53cf1b0d4e97f5a23db953e8de64c694
Dastan <dastanabitkulov901@gmail.com>
2026-01-02 04:56:14 +0600

    fix: main/urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from rest_framework.permissions import AllowAny
-
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Stripe API",
-        default_version="v1",
-        description="Django + Stripe test task",
-    ),
-    public=True,
-    permission_classes=[AllowAny],
-)
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", schema_view.with_ui("swagger", cache_timeout=0)),
 ]
--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
 from .views import (buy_item, item_page, success_page, 
                     cancel_page, buy_order, order_page)
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Stripe API",
+        default_version="v1",
+        description="Django + Stripe test task",
+    ),
+    public=True,
+    permission_classes=[AllowAny],
+)
 
 urlpatterns = [
     path("buy/<int:id>/", buy_item),
@@ -10,4 +23,5 @@ urlpatterns = [
     path("cancel", cancel_page),
     path("buy-order/<int:id>/", buy_order),
     path("order/<int:id>/", order_page),
+    path("", schema_view.with_ui("swagger", cache_timeout=0)),
 ]
